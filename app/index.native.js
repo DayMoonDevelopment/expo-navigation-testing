5a912c9651fab9682d44f830cd3a079efa088a52
Caleb Panza <45076058+calebpanza@users.noreply.github.com>
2024-07-10 11:45:22 -0400

    check in

---
+++ app/index.native.js
@@ -0,0 +1,14 @@
+import { Text, View } from "react-native";
+
+// 👇 Comment this out to get a working web bundle. Leave it imported and the web bundle will break.
+import MovToMp4 from "react-native-mov-to-mp4";
+
+function Index() {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>Native</Text>
+    </View>
+  );
+}
+
+export default Index;
---
+++ app/index.web.js
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+function Index() {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>Web</Text>
+    </View>
+  );
+}
+
+export default Index;
